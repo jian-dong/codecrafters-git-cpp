44b1f1c5a5ee4468ea699c50db3b1eb55edc17e6
Joe Dong <jian.dong@pobox.com>
2024-11-27 09:15:14 +0800

    uncoment code

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,50 +1,47 @@
-#include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <string>
 
-int main(int argc, char *argv[])
-{
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+int main(int argc, char *argv[]) {
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!\n";
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
 
-    // Uncomment this block to pass the first stage
-    //
-    // if (argc < 2) {
-    //     std::cerr << "No command provided.\n";
-    //     return EXIT_FAILURE;
-    // }
-    //
-    // std::string command = argv[1];
-    //
-    // if (command == "init") {
-    //     try {
-    //         std::filesystem::create_directory(".git");
-    //         std::filesystem::create_directory(".git/objects");
-    //         std::filesystem::create_directory(".git/refs");
-    //
-    //         std::ofstream headFile(".git/HEAD");
-    //         if (headFile.is_open()) {
-    //             headFile << "ref: refs/heads/main\n";
-    //             headFile.close();
-    //         } else {
-    //             std::cerr << "Failed to create .git/HEAD file.\n";
-    //             return EXIT_FAILURE;
-    //         }
-    //
-    //         std::cout << "Initialized git directory\n";
-    //     } catch (const std::filesystem::filesystem_error& e) {
-    //         std::cerr << e.what() << '\n';
-    //         return EXIT_FAILURE;
-    //     }
-    // } else {
-    //     std::cerr << "Unknown command " << command << '\n';
-    //     return EXIT_FAILURE;
-    // }
-    //
-    // return EXIT_SUCCESS;
+  if (argc < 2) {
+    std::cerr << "No command provided.\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string command = argv[1];
+
+  if (command == "init") {
+    try {
+      std::filesystem::create_directory(".git");
+      std::filesystem::create_directory(".git/objects");
+      std::filesystem::create_directory(".git/refs");
+
+      std::ofstream headFile(".git/HEAD");
+      if (headFile.is_open()) {
+        headFile << "ref: refs/heads/main\n";
+        headFile.close();
+      } else {
+        std::cerr << "Failed to create .git/HEAD file.\n";
+        return EXIT_FAILURE;
+      }
+
+      std::cout << "Initialized git directory\n";
+    } catch (const std::filesystem::filesystem_error &e) {
+      std::cerr << e.what() << '\n';
+      return EXIT_FAILURE;
+    }
+  } else {
+    std::cerr << "Unknown command " << command << '\n';
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
